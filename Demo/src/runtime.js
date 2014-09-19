d721e9a928eb906a06c04c2c0fdfb8b3200b9733
Elvis Attro <attroelvis@outlook.com>
2014-09-19 17:09:48 +0000

    New Master after Crashing past sid

---
+++ Demo/src/runtime.js
@@ -0,0 +1,105 @@
+function createSceneTuto(engine) {
+    //test atomdata to be remove later
+    //Creation of the scene
+    var scene = new BABYLON.Scene(engine);
+
+    // Adding light and camera
+    var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(10, 50, 50), scene);
+    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 40, new BABYLON.Vector3(0,0,0), scene);
+
+    // Adding new Ethanol molecule
+    var ethanol = new Molecule ("ethanol", "Ethanol", new BABYLON.Vector3(10,0,0), scene);
+    ethanol.position = new BABYLON.Vector3(0,0,0);
+
+   var eau = new Molecule ("eau", "Eau", new BABYLON.Vector3(10,0,0), scene);
+    eau.position = new BABYLON.Vector3(10,0,0);
+
+    /*var ground = BABYLON.Mesh.CreateGround("ground", 1000, 1000, 1, scene, false);
+    var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+    groundMaterial.specularColor = BABYLON.Color3.Black();
+    ground.material = groundMaterial;
+
+        // Events
+    var canvas = engine.getRenderingCanvas();
+    var startingPoint;
+    var currentMesh;
+
+    var getGroundPosition = function () {
+        // Use a predicate to get position on the ground
+        var pickinfo = scene.pick(scene.pointerX, scene.pointerY, function (mesh) { return mesh == ground; });
+        if (pickinfo.hit) {
+            return pickinfo.pickedPoint;
+        }
+
+        return null;
+    }
+
+ var onPointerDown = function (evt) {
+        if (evt.button !== 0) {
+            return;
+        }
+
+        // check if we are under a mesh
+        var pickInfo = scene.pick(scene.pointerX, scene.pointerY, function (mesh) { return mesh !== ground; });
+        if (pickInfo.hit) {
+            currentMesh = pickInfo.pickedMesh;
+            startingPoint = getGroundPosition(evt);
+
+            if (startingPoint) { // we need to disconnect camera from canvas
+                setTimeout(function () {
+                    camera.detachControl(canvas);
+                }, 0);
+            }
+        }
+    }
+
+    var onPointerUp = function () {
+        if (startingPoint) {
+            camera.attachControl(canvas);
+            startingPoint = null;
+            return;
+        }
+    }
+
+    var onPointerMove = function (evt) {
+        if (!startingPoint) {
+            return;
+        }
+
+        var current = getGroundPosition(evt);
+
+        if (!current) {
+            return;
+        }
+
+        var diff = current.subtract(startingPoint);
+        currentMesh.position.addInPlace(diff);
+
+        startingPoint = current;
+
+    }
+
+    canvas.addEventListener("pointerdown", onPointerDown, false);
+    canvas.addEventListener("pointerup", onPointerUp, false);
+    canvas.addEventListener("pointermove", onPointerMove, false);
+
+    scene.onDispose = function () {
+        canvas.removeEventListener("pointerdown", onPointerDown);
+        canvas.removeEventListener("pointerup", onPointerUp);
+        canvas.removeEventListener("pointermove", onPointerMove);
+    }
+*/
+
+    scene.registerBeforeRender(function () {
+
+    // lIGHT from camera anytime !
+        light.position = camera.position;
+
+        //animate the mesh
+        ethanol.rotation.x += 0.01;
+        ethanol.rotation.z += 0.02;
+    });
+
+return scene;
+
+}
